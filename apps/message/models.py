69bfdf914d72c574a43b8f0357564dc48bb258d9
guiyuan <email@example.com>
2017-05-12 16:21:11 +0800

    Initial commit

---
+++ apps/message/models.py
@@ -0,0 +1,14 @@
+#_*_ coding:utf-8_*_
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class UserMessage(models.Model):
+    name=models.CharField(max_length=20,verbose_name=u"用户名")
+    email=models.EmailField(verbose_name=u"邮箱")
+    address=models.CharField(max_length=100,verbose_name=u"联系地址")
+    message=models.CharField(max_length=500,verbose_name=u"说明")
+
+    class Meta:
+        verbose_name=u"用户留言信息"
